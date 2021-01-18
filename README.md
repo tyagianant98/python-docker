347e90a8d98edcd2da493c4035f81ca10076dcd9
Anant Tyagi <tyagianant98@gmail.com>
2021-01-18 23:06:37 +0530

    added dependencies

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # python-docker
 
-this is compueter
-hey, my name is anant
+- To deploy follow below instructions
+
+1. git clone this repo
+
+2. Make sure to install and configure Python3
+
+3. Install dependencies using command : pip install -r dependencies.txt
+
+4. run project : python server.py
+
+5. Access application on : http://localhost:8080/ping
---
+++ server.py
@@ -0,0 +1,25 @@
+
+from flask import Flask, render_template
+import socket
+import pandas as pd
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return f"Hello World"
+
+
+@app.route("/ping")
+def ping():
+    return f"pong"
+
+
+@app.route("/anant")
+def tyagi():
+    return render_template('anant.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int("8080"), debug=True)
